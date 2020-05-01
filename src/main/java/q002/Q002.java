81050e8af3fa220cc5e78618b2b9cb21b7b3186c
momokasatou <63344710+momokasatou@users.noreply.github.com>
2020-05-01 18:21:03 +0900

    002修正

--- src/main/java/q002/Q002.java
+++ src/main/java/q002/Q002.java
@@ -1,23 +1,19 @@
 package q002;
 
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 /**
  * Q002 並べ替える
  *
- * dataListに "ID,名字" の形式で20個のデータがあります。
- * これをID順に並べて表示するプログラムを記述してください。
+ * dataListに "ID,名字" の形式で20個のデータがあります。 これをID順に並べて表示するプログラムを記述してください。
  *
  * dataListの定義を変更してはいけません。
  *
  *
-[出力結果イメージ]
-1,伊藤
-2,井上
-（省略）
-9,清水
-10,鈴木
-11,高橋
-（省略）
-20,渡辺
+ * [出力結果イメージ] 1,伊藤 2,井上 （省略） 9,清水 10,鈴木 11,高橋 （省略） 20,渡辺
  */
 public class Q002 {
     /**
@@ -45,5 +41,23 @@ public class Q002 {
             "14,林",
             "9,清水"
     };
-}
-// 完成までの時間: xx時間 xx分
+
+    public static void main(String[] args){
+
+        List<String> list = Arrays.asList(dataList);
+
+        Map<Integer, String> map = new HashMap<>();
+
+        list.forEach(str -> {
+            String[] arr = str.split(",");
+            map.put(Integer.parseInt(arr[0]), arr[1]);
+        });
+
+        for (Map.Entry<Integer, String> name : map.entrySet()) {
+            System.out.println(name);
+        }
+    }
+};
+
+ 
+// 完成までの時間: 1時間 30分
